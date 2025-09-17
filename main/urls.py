c6ffa404a9fc98161e325f042eeeb135a4dcf774
Anindya <anindyaaaawt@gmail.com>
2025-09-17 13:55:57 +0700

    tambah semua html ui

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('user-story/', views.user_story, name='user_story'),
+    path('use-case/', views.use_case, name='use_case'),
+    path('scenario/', views.user_scenario, name='user_scenario'),
+    path('use-case-diagram/', views.use_case_diagram, name='use_case_diagram'),
+    path('use-case-spec/', views.use_case_spec, name='use_case_spec'),
+    path('activity-diagram/', views.activity_diagram, name='activity_diagram'),
+    path('sequence-diagram/', views.sequence_diagram, name='sequence_diagram'),
+    path('class-diagram/', views.class_diagram, name='class_diagram'),
+    path('generate-srs/', views.generate_srs, name='generate_srs'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,29 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+def user_story(request):
+    return render(request, 'main/user_story.html')
+
+def use_case(request):
+    return render(request, 'main/use_case.html')
+
+def user_scenario(request):
+    return render(request, 'main/user_scenario.html')
+
+def use_case_diagram(request):
+    return render(request, 'main/use_case_diagram.html')
+
+def use_case_spec(request):
+    return render(request, 'main/use_case_spec.html')
+
+def activity_diagram(request):
+    return render(request, 'main/activity_diagram.html')
+
+def sequence_diagram(request):
+    return render(request, 'main/sequence_diagram.html')
+
+def class_diagram(request):
+    return render(request, 'main/class_diagram.html')
+
+def generate_srs(request):
+    return render(request, 'main/generate_srs.html')
